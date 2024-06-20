fac03d9c377b652142bb2ad5cc593eb32cb5313d
Emiliano Garza <95396394+ChicoTecate@users.noreply.github.com>
2024-06-20 14:30:42 -0600

    Update AlgoritmoTT.py

--- AlgoritmoTT.py
+++ AlgoritmoTT.py
@@ -1,1 +1,44 @@
-goritm
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.datasets import mnist
+import numpy as np
+
+# Cargar el conjunto de datos MNIST
+(x_train, _), (x_test, _) = mnist.load_data()
+
+# Normalizar y remodelar los datos de entrada
+x_train = x_train.astype('float32') / 255.
+x_test = x_test.astype('float32') / 255.
+x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
+x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
+
+# Definir la arquitectura del autoencoder convolucional
+input_img = Input(shape=(28, 28, 1))
+x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = MaxPooling2D((2, 2), padding='same')(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+encoded = MaxPooling2D((2, 2), padding='same')(x)
+
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
+x = UpSampling2D((2, 2))(x)
+x = Conv2D(16, (3, 3), activation='relu')(x)
+x = UpSampling2D((2, 2))(x)
+decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+
+# Compilar el modelo
+autoencoder = Model(input_img, decoded)
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+# Entrenar el modelo
+autoencoder.fit(x_train, x_train,
+                epochs=10,
+                batch_size=128,
+                shuffle=True,
+                validation_data=(x_test, x_test))
+
+# Reconstruir las imágenes de prueba
+decoded_imgs = autoencoder.predict(x_test)
